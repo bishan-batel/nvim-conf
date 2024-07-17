cefe6288346898e4d54dbefe93fe87d71cb8c67a
Kishan S Patel <bishan.batel@protonmail.com>
2024-07-17 12:29:22 -0500

    Nvim

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,61 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  opts = {
+    options = {
+      theme = 'catppuccin',
+      section_separators = '',
+      component_separators = '',
+    },
+    sections = {
+      lualine_a = {
+        {
+          'filename',
+          path = 1,
+          separator = { left = '', right = '' },
+          color = { gui = 'bold' },
+          padding = 0,
+          shorting_target = 0,
+        },
+      },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {
+        {
+          '%l/%L,%c',
+          padding = 1,
+        },
+        {
+          'filetype',
+          padding = 0,
+        },
+      },
+    },
+    inactive_sections = {
+      lualine_a = {
+        {
+          'filename',
+          path = 1,
+          padding = 1,
+          shorting_target = 0,
+        },
+      },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {
+        {
+          '%l/%L,%c',
+          padding = 1,
+        },
+        {
+          'filetype',
+          padding = 0,
+        },
+      },
+    },
+  },
+}
